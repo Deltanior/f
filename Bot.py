0167c321ef124831b4e6732c2e9de3524310ef64
Deltanior <123987952+Deltanior@users.noreply.github.com>
2023-02-03 16:37:08 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -9,77 +9,155 @@ import os
 import asyncio
 
 
-bot ddseferfge5722882971:AAGYxdacv
+bot = Bot(token= ####)
 dp = Dispatcher(bot)
 
-rozklad_for_day = {}
-rozklad_for_p = {}
 
-nazva_paru_P = []
-pochatok_paru_P = []
-nazva_paru = []
-pochatok_paru = []
-nomer_paru = []
-rozklad_for_week = []
-cilka_na_paru = []
-cilka_na_paru_p = []
+rk =[0]
 
+
+rozklad_for_P = {}
+rozklad_for_V = {}
+rozklad_for_C = {}
+rozklad_for_Ch = {}
+rozklad_for_Py = {}
+rozklad_for_Cub = {}
+rozklad_for_N = {}
+
+list_para = []
 dict_para = {}
-rozklad = {}
 
-
-
-
-
+rozklad_for_week={}
+days = ['Понеділок', 'Вівторок', 'Середа', 'Четвер', 'П\'ятниця', 'Субота', 'Неділя']
 
 @dp.message_handler(commands=['Rozklad'])
 async def rjomba(message : types.Message):
-    await bot.send_message(message.from_user.id, rozklad_for_week[0])
+    x = 0
+    b = []
+    str = ''
+    str_1 = ''
+    k = 1
+    for i in days:
+        if days[x] in rozklad_for_week:
+            for i in rozklad_for_week[days[x]]:
+                a = (f'[{rozklad_for_week[days[x]][k][0]} \- {rozklad_for_week[days[x]][k][1]} \- {rozklad_for_week[days[x]][k][2]}]({rozklad_for_week[days[x]][k][3]})')
+                str += f'{a}\n'
+                k += 1
+            k = 1
+            str_1 +=f'{days[x]}\n{str}'
+            str = ''
+            x += 1
+        else:
+            x += 1
+    await bot.send_message(message.from_user.id, f'{str_1} ', parse_mode='MarkdownV2')
 
-
-@dp.message_handler(commands=['Pon'])
+@dp.message_handler(commands=['po'])
 async def rjomba(message : types.Message):
-    rozklad_for_p.update(rozklad_for_day)
-    nazva_paru_P.extend(nazva_paru)
-    pochatok_paru_P.extend(pochatok_paru)
-    rozklad_for_week.append(f'Понеділок\n{rozklad_for_p[1]}\n')
-    cilka_na_paru_p.extend(cilka_na_paru)
-    await bot.send_message(message.from_user.id, f'Понеділок\n{rozklad_for_p[1]}')
-    #await bot.send_message(message.from_user.id, cilka_na_paru_p)
-    rozklad_for_day.clear()
-    nazva_paru.clear()
-    pochatok_paru.clear()
-    nomer_paru.clear()
+    rozklad_for_P.update(dict_para)
+    rozklad_for_week_1 = {days[0]:rozklad_for_P}
+    rozklad_for_week.update(rozklad_for_week_1)
+    await bot.send_message(message.from_user.id, rozklad_for_P)
     dict_para.clear()
-    cilka_na_paru.clear()
-    #await bot.send_message(message.from_user.id, cilka_na_paru_p)
+    rk.clear()
+    rk.append(0)
 
-@dp.message_handler(commands=['ClearP'])
+@dp.message_handler(commands=['poc'])
+async def rjomba(message: types.Message):
+    rozklad_for_P.clear()
+
+@dp.message_handler(commands=['Vi'])
 async def rjomba(message : types.Message):
-    cilka_na_paru_p.clear()
-    rozklad_for_p.clear()
+    rozklad_for_V.update(dict_para)
+    rozklad_for_week_1 = {days[1]:rozklad_for_V}
+    rozklad_for_week.update(rozklad_for_week_1)
+    await bot.send_message(message.from_user.id, rozklad_for_V)
+    dict_para.clear()
+    rk.clear()
+    rk.append(0)
+
+@dp.message_handler(commands=['vic'])
+async def rjomba(message: types.Message):
+    rozklad_for_V.clear()
+
+@dp.message_handler(commands=['Ce'])
+async def rjomba(message : types.Message):
+    rozklad_for_C.update(dict_para)
+    rozklad_for_week_1 = {days[2]:rozklad_for_C}
+    rozklad_for_week.update(rozklad_for_week_1)
+    await bot.send_message(message.from_user.id, rozklad_for_C)
+    dict_para.clear()
+    rk.clear()
+    rk.append(0)
+
+@dp.message_handler(commands=['cec'])
+async def rjomba(message: types.Message):
+    rozklad_for_C.clear()
+
+@dp.message_handler(commands=['Ch'])
+async def rjomba(message : types.Message):
+    rozklad_for_Ch.update(dict_para)
+    rozklad_for_week_1 = {days[3]:rozklad_for_Ch}
+    rozklad_for_week.update(rozklad_for_week_1)
+    await bot.send_message(message.from_user.id, rozklad_for_Ch)
+    dict_para.clear()
+    rk.clear()
+    rk.append(0)
+
+@dp.message_handler(commands=['chc'])
+async def rjomba(message: types.Message):
+    rozklad_for_Ch.clear()
+
+
+@dp.message_handler(commands=['Py'])
+async def rjomba(message: types.Message):
+    rozklad_for_Py.update(dict_para)
+    rozklad_for_week_1 = {days[4]: rozklad_for_Py}
+    rozklad_for_week.update(rozklad_for_week_1)
+    await bot.send_message(message.from_user.id, rozklad_for_Py)
+    dict_para.clear()
+    rk.clear()
+    rk.append(0)
+
+
+@dp.message_handler(commands=['pyc'])
+async def rjomba(message: types.Message):
+    rozklad_for_Py.clear()
+
+
+@dp.message_handler(commands=['Cu'])
+async def rjomba(message: types.Message):
+    rozklad_for_Cub.update(dict_para)
+    rozklad_for_week_1 = {days[5]: rozklad_for_Cub}
+    rozklad_for_week.update(rozklad_for_week_1)
+    await bot.send_message(message.from_user.id, rozklad_for_Cub)
+    dict_para.clear()
+    rk.clear()
+    rk.append(0)
+
+
+@dp.message_handler(commands=['cuc'])
+async def rjomba(message: types.Message):
+    rozklad_for_Cub.clear()
+
+@dp.message_handler(commands=['Ne'])
+async def rjomba(message: types.Message):
+    rozklad_for_N.update(dict_para)
+    rozklad_for_week_1 = {days[6]: rozklad_for_N}
+    rozklad_for_week.update(rozklad_for_week_1)
+    await bot.send_message(message.from_user.id, rozklad_for_N)
+    dict_para.clear()
+    rk.clear()
+    rk.append(0)
+
+
+@dp.message_handler(commands=['nec'])
+async def rjomba(message: types.Message):
+    rozklad_for_N.clear()
+
+
+@dp.message_handler(commands=['Clearrozklad'])
+async def rjomba(message : types.Message):
     rozklad_for_week.clear()
-
-
-
-@dp.message_handler(commands=['p'])
-async def rjomba(message : types.Message):
-    await bot.send_message(message.from_user.id, f'Понеділок\n{rozklad_for_p[1]}')
-
-
-
-@dp.message_handler(commands=['pD'])
-async def rjomba(message: types.Message):
-    rozklad_for_p.clear()
-    #await bot.send_message(message.from_user.id,rozklad_for_p)
-
-
-@dp.message_handler(commands=['co'])
-async def rjomba(message: types.Message):
-    await bot.send_message(message.from_user.id, nazva_paru_P[0])
-    await bot.send_message(message.from_user.id, cilka_na_paru_p[0])
-    await bot.send_message(message.from_user.id, pochatok_paru_P)
-
 
 
 
@@ -89,43 +167,73 @@ async def Yebuddy13(message: types.Message):
     asyncio.create_task(Yebuddy())
 
 async def Yebuddy():
-    x = 0
-    for i in pochatok_paru_P:
-        aioschedule.every().day.at(pochatok_paru_P[x]).do(Yebuddy_1,x)
+    x = 1
+    for i in rozklad_for_week[days[0]]:
+        aioschedule.every().monday.at(rozklad_for_week[days[0]][x][2]).do(Yebuddy_1,x)
+        x += 1
+    x = 1
+    for i in rozklad_for_week[days[1]]:
+        aioschedule.every().tuesday.at(rozklad_for_week[days[1]][x][2]).do(Yebuddy_2, x)
+        x += 1
+    x = 1
+    for i in rozklad_for_week[days[2]]:
+        aioschedule.every().wednesday.at(rozklad_for_week[days[2]][x][2]).do(Yebuddy_3, x)
+        x += 1
+    x = 1
+    for i in rozklad_for_week[days[3]]:
+        aioschedule.every().thursday.at(rozklad_for_week[days[3]][x][2]).do(Yebuddy_4, x)
+        x += 1
+    x = 1
+    for i in rozklad_for_week[days[4]]:
+        aioschedule.every().friday.at(rozklad_for_week[days[4]][x][2]).do(Yebuddy_5, x)
+        x += 1
+    x = 1
+    for i in rozklad_for_week[days[5]]:
+        aioschedule.every().saturday.at(rozklad_for_week[days[5]][x][2]).do(Yebuddy_6, x)
+        x += 1
+    x = 1
+    for i in rozklad_for_week[days[6]]:
+        aioschedule.every().sunday.at(rozklad_for_week[days[6]][x][2]).do(Yebuddy_7, x)
         x += 1
     while True:
         await aioschedule.run_pending()
         await asyncio.sleep(1)
 
 async def Yebuddy_1(x):
-    await bot.send_message(chat_id=518018061, text=f'[{nazva_paru_P[x]}]({cilka_na_paru_p[x]})',parse_mode='MarkdownV2')
+    await bot.send_message(chat_id=518018061, text=f'[{rozklad_for_week[days[0]][x][1]}]({rozklad_for_week[days[0]][x][3]})',parse_mode='MarkdownV2')
 
+async def Yebuddy_2(x):
+    await bot.send_message(chat_id=518018061, text=f'[{rozklad_for_week[days[1]][x][1]}]({rozklad_for_week[days[1]][x][3]})',parse_mode='MarkdownV2')
 
+async def Yebuddy_3(x):
+    await bot.send_message(chat_id=518018061, text=f'[{rozklad_for_week[days[2]][x][1]}]({rozklad_for_week[days[2]][x][3]})',parse_mode='MarkdownV2')
 
+async def Yebuddy_4(x):
+    await bot.send_message(chat_id=518018061, text=f'[{rozklad_for_week[days[3]][x][1]}]({rozklad_for_week[days[3]][x][3]})',parse_mode='MarkdownV2')
 
+async def Yebuddy_5(x):
+    await bot.send_message(chat_id=518018061, text=f"[{rozklad_for_week[days[4]][x][1]}]({rozklad_for_week[days[4]][x][3]})",parse_mode='MarkdownV2')
+
+async def Yebuddy_6(x):
+    await bot.send_message(chat_id=518018061, text=f'[{rozklad_for_week[days[5]][x][1]}]({rozklad_for_week[days[5]][x][3]})',parse_mode='MarkdownV2')
+
+async def Yebuddy_7(x):
+    await bot.send_message(chat_id=518018061, text=f'[{rozklad_for_week[days[6]][x][1]}]({rozklad_for_week[days[6]][x][3]})',parse_mode='MarkdownV2')
 
 
 
 @dp.message_handler()
 async def rjomba(message : types.Message):
+
     text_from_user = message.text
     forma_for_zchituvanya = re.compile(r'\d+\s+\w+\s+\d\d:\d\d+.*')
     new_text = forma_for_zchituvanya.search(text_from_user)
     new_text_spicok = new_text.group().split()
-    para = {f"{new_text_spicok[0]}":f"{new_text_spicok[1]} {new_text_spicok[2]} {new_text_spicok[3]}"}
+    list_para = [new_text_spicok[0],new_text_spicok[1],new_text_spicok[2],new_text_spicok[3]]
+
+    rk[0] += 1
+    para = {rk[0] : list_para}
     dict_para.update(para)
-    nomer_paru.append(new_text_spicok[0])
-    nazva_paru.append(new_text_spicok[1])
-    pochatok_paru.append(new_text_spicok[2])
-    cilka_na_paru.append(new_text_spicok[3])
-    string_for_day = ''
-    x = 0
-    for i in dict_para:
-        b = dict_para[f'{nomer_paru[x]}']
-        string_for_day += f'{nomer_paru[x]}-{nazva_paru[x]}\n'
-        rozklad_for_day_1 = {1: string_for_day}
-        rozklad_for_day.update(rozklad_for_day_1)
-        x += 1
 
     await bot.send_message(message.from_user.id, dict_para)
 
